f98129a2357df2553b469939b258f986a16a91c3
Guilherme Ferraz <guilherme.ferraz@portergroup.com.br>
2022-05-09 14:01:03 -0300

    feat: add start and restart match

--- QUEAH.py
+++ QUEAH.py
@@ -21,6 +21,7 @@ POSICOES_INICIAIS_AZUL = [[0, 1], [3, 2], [2, 1], [1, 2]]
 
 class Application:
     def __init__(self, master=None):
+        self.iniciarJogo()
         self.jogador1 = PhotoImage(file=f"jogador1.png")
         self.jogador2 = PhotoImage(file=f"jogador2.png")
         self.casaTabuleiro = PhotoImage(file=f"peça.png")
@@ -53,6 +54,10 @@ class Application:
             master, text="Peças faltantes: 6", font=MEDIUMFONT)
         piecesLeft2.place(relx=0.93, rely=0.5, anchor=CENTER)
 
+        piecesLeft2 = Button(
+            master, text="Reiniciar partida", font=MEDIUMFONT, command=self.reiniciarPartida)
+        piecesLeft2.place(relx=0.9, rely=0.05, anchor=CENTER)
+
         for i in range(5):
             for j in range(5):
                 if (i % 2) == 0:
@@ -61,38 +66,44 @@ class Application:
                             peca = Label(self.tabuleiro,
                                          image=self.pinoAzul)
                             peca.place(x=52 * j + 120, y=50 * i + 81)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.pinoAzul
                         elif [i, j] in POSICOES_INICIAIS_VERMELHO:
                             peca = Label(self.tabuleiro,
                                          image=self.pinoVermelho)
                             peca.place(x=52 * j + 120, y=50 * i + 81)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.pinoVermelho
                         else:
                             peca = Label(self.tabuleiro,
                                          image=self.casaTabuleiro)
                             peca.place(x=52 * j + 120, y=50 * i + 81)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.casaTabuleiro
                 else:
                     if (j in CASAS_POSSIVEIS_IMPAR):
                         if [i, j] in POSICOES_INICIAIS_AZUL:
                             peca = Label(self.tabuleiro, image=self.pinoAzul)
                             peca.place(x=50 * j + 98, y=50 * i + 80)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.pinoAzul
                         elif [i, j] in POSICOES_INICIAIS_VERMELHO:
                             peca = Label(self.tabuleiro,
                                          image=self.pinoVermelho)
                             peca.place(x=50 * j + 98, y=50 * i + 80)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.pinoVermelho
                         else:
                             peca = Label(self.tabuleiro,
                                          image=self.casaTabuleiro)
                             peca.place(x=50 * j + 98, y=50 * i + 80)
-                            peca.bind("<Button-1>", lambda e: print('Clicado'))
+                            peca.bind("<Button-1>",
+                                      lambda e: self.casaClicada())
                             peca.image = self.casaTabuleiro
                         peca = Label(self.tabuleiro, image=self.casaTabuleiro)
 
@@ -102,6 +113,14 @@ class Application:
     def finalizarJogo(self):
         messagebox.showinfo(
             "showinfo", "Jogador 1 venceu!! Clique para jogar novamente.")
+
+    def reiniciarPartida(self):
+        messagebox.showinfo(
+            "showinfo", "Partida reiniciada.")
+
+    def casaClicada(self):
+        messagebox.showinfo(
+            "showinfo", "Uma casa do tabuleiro foi clicada.")
 
 
 root = Tk()
