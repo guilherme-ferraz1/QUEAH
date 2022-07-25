b84eb2f8e870030721d6fa5b89b5f04693d883b4
GutierrezGtz <matheus.villegas@gmail.com>
2022-07-25 00:00:28 -0300

    class Status

---
+++ Status.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Status(Enum):
+    
+    LIVRE = 'LIVRE'
+    OCUPADA_1 = 'OCUPADA_1'
+    OCUPADA_2 = 'OCUPADA_2'
+    INATIVO = 'INATIVO'
+    RESERVA_1 = 'RESERVA_1'
+    RESERVA_2 = 'RESERVA_2'
